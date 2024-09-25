d0e1993545e2bf8403508f436de7095991ae2f62
Ruixi Zhou <zhouruixi@gmail.com>
2024-09-25 23:40:03 +0800

    Add gadget usb net configuration
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- gs.sh
+++ gs.sh
@@ -4,14 +4,24 @@ source /config/gs.conf
 
 # Auto expand rootfs [ TODO ]
 # If the partition has been extended, an expanded file will be created. If the file is detected to exist, the automatic partition extension will be skipped.
+
+# eth0 network configuration
 [ -f /etc/NetworkManager/system-connections/eth0.nmconnection ] || nmcli con add type ethernet con-name eth0 ifname eth0 ipv4.method auto ipv4.addresses ${ETH_Fixed_ip},${ETH_Fixed_ip2} autoconnect yes
 if [[ -n $ETH_Fixed_ip && -n $ETH_Fixed_ip2 ]]; then
 	# Check whether the configuration in gs.conf is consistent with eth0. If not, update it.
-	ETH_Fixed_ip_OS=$(nmcli -g ipv4.addresses  con show eth0)
-	[ $ETH_Fixed_ip_OS == "${ETH_Fixed_ip}, ${ETH_Fixed_ip2}" ] || nmcli con modify eth0 ipv4.addresses ${ETH_Fixed_ip},${ETH_Fixed_ip2}
+	eth0_fixed_ip_OS=$(nmcli -g ipv4.addresses con show eth0)
+	[ $eth0_fixed_ip_OS == "${ETH_Fixed_ip}, ${ETH_Fixed_ip2}" ] || nmcli con modify eth0 ipv4.addresses ${ETH_Fixed_ip},${ETH_Fixed_ip2}
 fi
 
+# radxa0 network configuration
+[ -f /etc/NetworkManager/system-connections/radxa0.nmconnection ] || nmcli con add type ethernet con-name radxa0 ifname radxa0 ipv4.method auto ipv4.addresses ${gadget_net_fixed_ip} autoconnect yes
+if [[ -n $gadget_net_fixed_ip ]]; then
+	# Check whether the configuration in gs.conf is consistent with radxa0. If not, update it.
+	radxa0_fixed_ip_OS=$(nmcli -g ipv4.addresses con show radxa0)
+	[ $radxa0_fixed_ip_OS == "${gadget_net_fixed_ip}" ] || nmcli con modify radxa0 ipv4.addresses ${gadget_net_fixed_ip}
+fi
 
+# wlan0 network card configuration
 # If no connection named radxa, create one to automatically connect to the unencrypted WiFi named OpenIPC.
 [ -f /etc/NetworkManager/system-connections/radxa.nmconnection ] || nmcli con add type wifi ifname wlan0 con-name radxa ssid OpenIPC
 # If the WiFi configuration in gs.conf is not empty and changes, modify the WiFi connection information according to the configuration file
