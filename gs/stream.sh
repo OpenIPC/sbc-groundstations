cabdf5626095de43f916b3e3091aeaa28a1ab924
Ruixi Zhou <zhouruixi@gmail.com>
2024-10-12 23:03:49 +0800

    optimization record button code
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- gs/stream.sh
+++ gs/stream.sh
@@ -36,42 +36,40 @@ function gencmd(){
 gencmd norecord
 bash -c "$video_play_cmd" &
 pid_player=$!
-while true; do
-	if [ "$(gpiomon -F %e -n 1 $(gpiofind PIN_${REC_GPIO_PIN}))" == "1" ]; then
-		if [ "$video_record" == "0" ]; then
-			kill -15 $pid_player
-			sleep 0.2
-			# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
-			# gencmd record_${current_date}.ts
-			rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mp4)" | tail -n 1)
-			if [ -z $rec_index ]; then
-				rec_index="1000"
-			else
-				rec_index=$(($rec_index + 1))
-			fi
-			gencmd ${rec_index}.mp4
-			bash -c "$video_rec_cmd" &
-			pid_player=$!
-			video_record='1'
-			# Todo: How to deal with led when system is overheat
-			(
-			while true; do
-				# Blink red record LED
-				gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=1
-				gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=0
-			done
-		        ) &
-			pid_led=$!
+while [ "$(gpiomon -F %e -n 1 $(gpiofind PIN_${REC_GPIO_PIN}))" == "1" ]; do
+	if [ "$video_record" == "0" ]; then
+		kill -15 $pid_player
+		sleep 0.2
+		# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
+		# gencmd record_${current_date}.ts
+		rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mp4)" | tail -n 1)
+		if [ -z $rec_index ]; then
+			rec_index="1000"
 		else
-			kill -15 $pid_player
-			[ -z $pid_led ] || kill $pid_led
-			sleep 0.2
-			sleep 1 && gpioset -D $REC_LED_drive $(gpiofind PIN_${REC_LED_PIN})=0 &
-			gencmd norecord
-			bash -c "$video_play_cmd" &
-			pid_player=$!
-			video_record='0'
+			rec_index=$(($rec_index + 1))
 		fi
+		gencmd ${rec_index}.mp4
+		bash -c "$video_rec_cmd" &
+		pid_player=$!
+		video_record='1'
+		# Todo: How to deal with led when system is overheat
+		(
+		while true; do
+			# Blink red record LED
+			gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=1
+			gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=0
+		done
+		) &
+		pid_led=$!
+	else
+		kill -15 $pid_player
+		[ -z $pid_led ] || kill $pid_led
+		sleep 0.2
+		sleep 1 && gpioset -D $REC_LED_drive $(gpiofind PIN_${REC_LED_PIN})=0 &
+		gencmd norecord
+		bash -c "$video_play_cmd" &
+		pid_player=$!
+		video_record='0'
 	fi
 	sleep 1
 done
