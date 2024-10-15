be7ab1ec9cf33946906a61cf24572ceea39707fb
Ruixi Zhou <zhouruixi@gmail.com>
2024-10-15 17:18:21 +0800

    Start or stop recording without interrupting the video with PixelPilot_rk
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- gs/stream.sh
+++ gs/stream.sh
@@ -9,15 +9,13 @@ cd $REC_Dir
 video_record="0"
 video_play_cmd=""
 video_rec_cmd=""
+GPIO_REC=$(gpiofind PIN_${REC_GPIO_PIN})
+GPIO_REC_LED=$(gpiofind PIN_${REC_LED_PIN})
 
 function gencmd(){
 	if [ "$video_player" == "pixelpilot" ]; then
-		video_rec_cmd="pixelpilot --screen-mode $SCREEN_MODE --codec $video_codec --dvr-framerate $REC_FPS --dvr-fmp4 --dvr $1"
-		video_play_cmd="pixelpilot --screen-mode $SCREEN_MODE --codec $video_codec"
-		if [ "$osd_enable" == "yes" ];then
-			video_rec_cmd="$video_rec_cmd --osd --osd-elements $osd_elements --osd-telem-lvl $osd_telem_lvl"
-			video_play_cmd="$video_play_cmd --osd --osd-elements $osd_elements --osd-telem-lvl $osd_telem_lvl"
-		fi
+		video_play_cmd="pixelpilot --screen-mode $SCREEN_MODE --codec $video_codec --dvr-framerate $REC_FPS --dvr-fmp4 --dvr-template ${REC_Dir}/record_%Y-%m-%d_%H-%M-%S.mp4"
+		[ "$osd_enable" == "yes" ] && video_play_cmd="$video_play_cmd --osd --osd-elements $osd_elements --osd-telem-lvl $osd_telem_lvl"
 	elif [ "$video_player" == "gstreamer" ]; then
 		video_play_cmd="gst-launch-1.0 -e udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H${video_codec:1:4}' ! rtp${video_codec}depay ! ${video_codec}parse ! mppvideodec ! kmssink"
 		video_rec_cmd="gst-launch-1.0 -e udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H${video_codec:1:4}' ! rtp${video_codec}depay ! ${video_codec}parse ! tee name=t ! mppvideodec ! kmssink t. ! queue ! mp4mux ! filesink location=${1}"
@@ -36,39 +34,48 @@ function gencmd(){
 gencmd norecord
 bash -c "$video_play_cmd" &
 pid_player=$!
-while [ "$(gpiomon -F %e -n 1 $(gpiofind PIN_${REC_GPIO_PIN}))" == "1" ]; do
+while [ "$(gpiomon -F %e -n 1 ${GPIO_REC})" == "1" ]; do
 	if [ "$video_record" == "0" ]; then
-		kill -15 $pid_player
-		sleep 0.2
-		# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
-		# gencmd record_${current_date}.ts
-		rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mp4)" | tail -n 1)
-		if [ -z $rec_index ]; then
-			rec_index="1000"
+		if [ "$video_player" == "pixelpilot" ]; then
+			kill -SIGUSR1 $pid_player
 		else
-			rec_index=$(($rec_index + 1))
+			kill -15 $pid_player
+			sleep 0.2
+			# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
+			# gencmd record_${current_date}.ts
+			rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mp4)" | tail -n 1)
+			if [ -z $rec_index ]; then
+				rec_index="1000"
+			else
+				rec_index=$(($rec_index + 1))
+			fi
+			gencmd ${rec_index}.mp4
+			bash -c "$video_rec_cmd" &
+			pid_player=$!
 		fi
-		gencmd ${rec_index}.mp4
-		bash -c "$video_rec_cmd" &
-		pid_player=$!
 		video_record='1'
 		# Todo: How to deal with led when system is overheat
 		(
 		while true; do
 			# Blink red record LED
-			gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=1
-			gpioset -D $REC_LED_drive -m time -s 1 $(gpiofind PIN_${REC_LED_PIN})=0
+			gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=1
+			gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=0
 		done
 		) &
 		pid_led=$!
 	else
-		kill -15 $pid_player
+		# turn off record LED
 		[ -z $pid_led ] || kill $pid_led
-		sleep 0.2
-		sleep 1 && gpioset -D $REC_LED_drive $(gpiofind PIN_${REC_LED_PIN})=0 &
-		gencmd norecord
-		bash -c "$video_play_cmd" &
-		pid_player=$!
+		sleep 1.2 && gpioset -D $REC_LED_drive ${GPIO_REC_LED}=0 &
+		if [ "$video_player" == "pixelpilot" ]; then
+			kill -SIGUSR1 $pid_player
+		else
+			kill -15 $pid_player
+			sleep 0.2
+			gencmd norecord
+			bash -c "$video_play_cmd" &
+			pid_player=$!
+		fi
 		video_record='0'
 	fi
 	sleep 1
