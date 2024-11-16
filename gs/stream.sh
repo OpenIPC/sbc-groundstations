01b86b6cda77c22247edef3bad59c4211ade300f
Ruixi Zhou <zhouruixi@gmail.com>
2024-11-16 22:50:46 +0800

    support long press to cleanp record files
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- gs/stream.sh
+++ gs/stream.sh
@@ -26,6 +26,16 @@ function gencmd(){
 	fi
 }
 
+function check_record_freespace() {
+	local rec_dir_freespace=$(df $REC_Dir | tail -n 1 | awk '{print $4}')
+	local rec_dir_freespace_MB=$((${rec_dir_freespace} / 1024))
+	if [ $rec_dir_freespace_MB -lt $rec_dir_freespace_min ]; then
+		echo "insufficient"
+	else
+		echo "sufficient"
+	fi
+}
+
 gencmd norecord
 bash -c "$video_play_cmd" &
 pid_player=$!
@@ -33,56 +43,86 @@ while gpiomon -r -s -n 1 -B pull-down ${GPIO_REC}; do
 	# do a 50ms simple software de-bounce
 	sleep 0.05
 	[ "$(gpioget $GPIO_REC)" == "1" ] || continue
-	if [ "$video_record" == "0" ]; then
-		rec_dir_freespace=$(df $REC_Dir | grep $REC_Dir | awk '{print $4}')
-		rec_dir_freespace_MB=$((${rec_dir_freespace} / 1024))
-		if [ $rec_dir_freespace_MB -lt $rec_dir_freespace_min ]; then
-			echo "No enough record space!" > /run/pixelpilot.msg
-			continue
-		fi
-		if [ "$video_player" == "pixelpilot" ]; then
-			kill -SIGUSR1 $pid_player
-		else
-			kill -15 $pid_player
-			sleep 0.2
-			# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
-			# gencmd record_${current_date}.ts
-			rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mkv)" | tail -n 1)
-			if [ -z $rec_index ]; then
-				rec_index="1000"
-			else
-				rec_index=$(($rec_index + 1))
+	button_press_uptime=$(cut -d ' ' -f 1 /proc/uptime | tr -d .)
+	gpiomon -f -s -n 1 -B pull-down ${GPIO_REC}
+	button_release_uptime=$(cut -d ' ' -f 1 /proc/uptime | tr -d .)
+	button_pressed_time=$((${button_release_uptime} - ${button_press_uptime}))
+	if [ $button_pressed_time -lt 200 ]; then
+		if [ "$video_record" == "0" ]; then
+			if [ "$(check_record_freespace)" == "insufficient" ]; then
+				echo "No enough record space!" > /run/pixelpilot.msg
+				continue
 			fi
-			gencmd ${rec_index}.mkv
-			bash -c "$video_rec_cmd" &
-			pid_player=$!
-		fi
-		echo "record start!" > /run/pixelpilot.msg
-		video_record='1'
-		# Todo: How to deal with led when system is overheat
-		(
-		while true; do
-			# Blink red record LED
-			gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=1
-			gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=0
-		done
-		) &
-		pid_led=$!
-	else
-		# turn off record LED
-		[ -z $pid_led ] || kill $pid_led
-		sleep 1.2 && gpioset -D $REC_LED_drive ${GPIO_REC_LED}=0 &
-		if [ "$video_player" == "pixelpilot" ]; then
-			kill -SIGUSR1 $pid_player
+			if [ "$video_player" == "pixelpilot" ]; then
+				kill -SIGUSR1 $pid_player
+			else
+				kill -15 $pid_player
+				sleep 0.2
+				# current_date=$(date +'%m-%d-%Y_%H-%M-%S')
+				# gencmd record_${current_date}.ts
+				rec_index=$(ls -1 $REC_Dir | grep -oP "^\d+(?=\.mkv)" | tail -n 1)
+				if [ -z $rec_index ]; then
+					rec_index="1000"
+				else
+					rec_index=$(($rec_index + 1))
+				fi
+				gencmd ${rec_index}.mkv
+				bash -c "$video_rec_cmd" &
+				pid_player=$!
+			fi
+			echo "record start!" > /run/pixelpilot.msg
+			video_record='1'
+			# Todo: How to deal with led when system is overheat
+			(
+			while true; do
+				# Blink red record LED
+				gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=1
+				gpioset -D $REC_LED_drive -m time -s 1 ${GPIO_REC_LED}=0
+			done
+			) &
+			pid_led=$!
 		else
-			kill -15 $pid_player
-			sleep 0.2
-			gencmd norecord
-			bash -c "$video_play_cmd" &
-			pid_player=$!
+			# turn off record LED
+			[ -z $pid_led ] || kill $pid_led
+			sleep 1.2 && gpioset -D $REC_LED_drive ${GPIO_REC_LED}=0 &
+			if [ "$video_player" == "pixelpilot" ]; then
+				kill -SIGUSR1 $pid_player
+			else
+				kill -15 $pid_player
+				sleep 0.2
+				gencmd norecord
+				bash -c "$video_play_cmd" &
+				pid_player=$!
+			fi
+			echo "record stop!" > /run/pixelpilot.msg
+			video_record='0'
 		fi
-		echo "record stop!" > /run/pixelpilot.msg
-		video_record='0'
+		sleep 3
+	elif [ $button_pressed_time -ge 200 ]; then
+		# first long press cleanup record until have enough space
+		# secord long press in 60s remove all record files
+		record_file_list=$(find $REC_Dir -maxdepth 1 -type f \( -name '*.mp4' -o -name '*.mkv' \))
+		if [ -n "$record_file_list" ];then
+			if [ ! -f ${REC_Dir}/cleanup_record ]; then
+				for record_file in $record_file_list; do
+					[ "$(check_record_freespace)" == "sufficient" ] && break
+					rm $record_file
+				done
+				echo "cleanup record done!" > /run/pixelpilot.msg
+				(
+				touch ${REC_Dir}/cleanup_record
+				sleep 60
+				[ -f ${REC_Dir}/cleanup_record ] && rm ${REC_Dir}/cleanup_record
+				) &
+			else
+				for record_file in $record_file_list; do
+					rm $record_file
+				done
+				[ -f ${REC_Dir}/cleanup_record ] && rm ${REC_Dir}/cleanup_record
+				echo "All record file deleted!" > /run/pixelpilot.msg
+			fi
+		else
+			echo "no record file found!" > /run/pixelpilot.msg
+		fi
 	fi
-	sleep 3
 done
