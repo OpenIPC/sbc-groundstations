27b6670482680746445987d3fc029287bbc7d58d
Ruixi Zhou <zhouruixi@gmail.com>
2025-02-22 21:18:25 +0800

    Enable color support of ls and grep, add handy aliases.
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -192,6 +192,25 @@ sed -i "/ListenStream=\[::1\]:2947/s/^/# /" /lib/systemd/system/gpsd.socket
 # set chrony use gps time
 echo "refclock SHM 0 refid GPS offset 0.1 delay 0.1" >> /etc/chrony/chrony.conf
 
+# enable color support of ls and also add handy aliases
+cat >> /etc/bash.bashrc << EOF
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+EOF
+
 # Forward SBC port 2222/8080 to IPC port 22/80
 cat > /etc/iptables/rules.v4 << EOF
 *nat
