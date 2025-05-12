777ee88f6135663bd61567b5eb2cfe5af56ebeb8
Ruixi Zhou <zhouruixi@gmail.com>
2025-05-12 11:05:28 +0800

    Bump RubyFPV to 11.1
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -155,16 +155,15 @@ cp -a osd.rockchip /usr/local/bin/wfb-ng-osd
 popd
 
 # RubyFPV
-rubyfpv_version="11.0"
+rubyfpv_version="11.1"
 apt -y install build-essential librockchip-mpp-dev libdrm-dev libcairo-dev libpcap-dev libi2c-dev libgpiod-dev wireless-tools
 git clone --depth=1 --branch $rubyfpv_version --single-branch https://github.com/RubyFPV/RubyFPV.git
 pushd RubyFPV
 make all RUBY_BUILD_ENV=radxa
 
 mkdir -p /home/radxa/ruby/plugins/osd
-cp -a ruby_* test_* res /home/radxa/ruby
+cp -a ruby_* test_* res version_ruby_base.txt /home/radxa/ruby
 cp -a ruby_plugin_* /home/radxa/ruby/plugins/osd
-[ -f version_ruby_base.txt ] && cp version_ruby_base.txt /home/radxa/ruby
 popd
 
 # SBC-GS-CC
