e034eabbc76c83c9776e878e60629ee3a37c5965
Ruixi Zhou <zhouruixi@gmail.com>
2025-11-27 00:26:48 +0800

    Add radxa zero3 and as base board
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- board/radxa/zero3/pre-build.sh
+++ board/radxa/zero3/pre-build.sh
@@ -10,7 +10,7 @@ then
     ./bsp linux rk356x --no-build
     sed -i 's/^#"radxa-zero3"/"radxa-zero3"/' u-boot/latest/fork.conf
     ./bsp u-boot latest radxa-zero3 --no-build
-    patch -p1 -i ${BR2_EXTERNAL_OPENIPC_SBC_GS_PATH}/board/runcam/wifilink/0001-uboot-compile.patch
+    patch -p1 -i ${BR2_EXTERNAL_OPENIPC_SBC_GS_PATH}/board/radxa/zero3/0001-uboot-compile.patch
 else
     echo "bsp already downloaded, skipping ..."
 fi
