bbe5b259803cd8c10b64d546832e543a0e386a1d
Ruixi Zhou <zhouruixi@gmail.com>
2025-11-27 00:26:48 +0800

    Add radxa zero3 and as base board
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- external.mk
+++ external.mk
@@ -1,4 +1,4 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_OPENIPC_SBC_GS_PATH)/package/*/*.mk))
 ifeq ($(BR2_PACKAGE_HOST_RKDEVELOPTOOL),y)
-include $(BR2_EXTERNAL_OPENIPC_SBC_GS_PATH)/board/runcam/wifilink/flash.mk
+include $(BR2_EXTERNAL_OPENIPC_SBC_GS_PATH)/board/radxa/zero3/flash.mk
 endif
