85544b474f0bfe0cce11a01a7c7a42ce39487f5e
Henk Wiedig <henk@wiedig.de>
2025-12-02 21:37:27 +0100

    fix gitlab runner build (#41)

--- board/radxa/zero3/local.mk
+++ board/radxa/zero3/local.mk
@@ -9,3 +9,23 @@ define BUSYBOX_APPLY_CUSTOM_PATCHES
 endef
 
 BUSYBOX_POST_PATCH_HOOKS += BUSYBOX_APPLY_CUSTOM_PATCHES
+
+# We have tight space constraints on the gitlab runners
+ifeq ($(CI),true)
+
+# Remove unused build artifacs after install
+define CI_CLEANUP_HOOK
+	@echo "CI: Cleaning build directory to save space"
+	$(RM) -r $(@D)/*
+endef
+LINUX_FIRMWARE_POST_INSTALL_IMAGES_HOOKS += CI_CLEANUP_HOOK
+SAMBA4_POST_INSTALL_TARGET_HOOKS += CI_CLEANUP_HOOK
+
+# Remove unused src tree
+define CI_CLEANUP_SRC_HOOK
+	@echo "CI: Cleaning src directory to save space"
+	$(RM) -r $(SRCDIR)
+endef
+LINUX_POST_RSYNC_HOOKS += CI_CLEANUP_SRC_HOOK
+
+endif
