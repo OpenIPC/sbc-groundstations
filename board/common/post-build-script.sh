926bf21e3e56203dce7e5b02ff96e402e8f37889
CC <zhouruixi@gmail.com>
2025-12-06 14:17:20 +0800

    upload development build to buildroot-snapshot tag release (#46)
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- board/common/post-build-script.sh
+++ board/common/post-build-script.sh
@@ -3,7 +3,7 @@
 eval $(grep BR2_DEFCONFIG ${O}/.config)
 echo "BUILD_CONFIG=$(basename $(basename $BR2_DEFCONFIG) _defconfig)" >> $TARGET_DIR/etc/os-release
 
-DOWNLOAD_URL="${GITHUB_SERVER_URL:-https://github.com}/${GITHUB_REPOSITORY:-OpenIPC/sbc-groundstations}/releases/download/latest/$(basename $(basename $BR2_DEFCONFIG) _defconfig).tar.gz"
+DOWNLOAD_URL="${GITHUB_SERVER_URL:-https://github.com}/${GITHUB_REPOSITORY:-OpenIPC/sbc-groundstations}/releases/download/buildroot-snapshot/$(basename $(basename $BR2_DEFCONFIG) _defconfig).tar.gz"
 echo "UPGRADE=$DOWNLOAD_URL" >> $TARGET_DIR/etc/os-release
 
 echo "BUILD_DATE=\"$(date)\"" >> $TARGET_DIR/etc/os-release
