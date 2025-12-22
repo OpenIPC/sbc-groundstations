1056d8f76cc6e1ee032e14eb0783bf743c75b454
Henk Wiedig <henk@wiedig.de>
2025-12-22 20:22:50 +0100

    use kconfig instead of patches, use HOST python3

--- board/radxa/zero3/local.mk
+++ board/radxa/zero3/local.mk
@@ -3,14 +3,14 @@ LINUX_CFLAGS = "-Wno-enum-int-mismatch"
 UBOOT_OVERRIDE_SRCDIR=$(BUILD_DIR)/radxa-bsp-main/.src/u-boot
 UBOOT_OVERRIDE_SRCDIR=$(BUILD_DIR)/radxa-bsp-main/.src/u-boot
 ROCKCHIP_RKBIN_OVERRIDE_SRCDIR=$(BUILD_DIR)/radxa-bsp-main/.src/rkbin
-LINUX_DEPENDENCIES += wireless-regdb
 
 REGDB_CUSTOM_TXT := $(BR2_EXTERNAL_OPENIPC_SBC_GS_PATH)/board/radxa/zero3/db.txt
 
 define WIRELESS_REGDB_USE_CUSTOM_DB_AND_REGEN_DB
 	@echo "wireless-regdb: using custom db.txt and regenerating regulatory.db (unsigned)"
 	$(INSTALL) -m 0644 $(REGDB_CUSTOM_TXT) $(@D)/db.txt
-	$(MAKE) -C $(@D) regulatory.db
+	cd $(@D) && \
+		$(HOST_DIR)/bin/python3 ./db2fw.py regulatory.db db.txt
 	ls -l $(@D)/regulatory.db
 endef
 WIRELESS_REGDB_POST_PATCH_HOOKS += WIRELESS_REGDB_USE_CUSTOM_DB_AND_REGEN_DB
@@ -42,17 +42,6 @@ LINUX_POST_RSYNC_HOOKS += CI_CLEANUP_SRC_HOOK
 
 endif
 
-define LINUX_RK_REGDB_AFTER_RSYNC
-	@echo "Post-rsync: apply patches + install db + force kconfig"
-
-	$(APPLY_PATCHES) $(@D) \
-		$(BR2_EXTERNAL_OPENIPC_SBC_GS_PATH)/board/radxa/zero3/linux-patches \
-		\*.patch
-
-endef
-LINUX_POST_RSYNC_HOOKS += LINUX_RK_REGDB_AFTER_RSYNC
-
-
 define LINUX_REGDB_COPY_FOR_BUILTIN_FW
 	@echo "Copy regulatory.db into kernel objtree for built-in firmware"
 	@obj="$(@D)"; \
@@ -65,15 +54,3 @@ define LINUX_REGDB_COPY_FOR_BUILTIN_FW
 	test -f "$$obj/firmware/regulatory.db"
 endef
 LINUX_POST_CONFIGURE_HOOKS += LINUX_REGDB_COPY_FOR_BUILTIN_FW
-
-
-define LINUX_REGDB_FORCE_BUILTIN_FW_CONFIG
-	@echo "Force built-in firmware for regulatory.db (and regenerate config)"
-
-	$(SED) '/^\(# \)\?CONFIG_EXTRA_FIRMWARE\>/d' $(@D)/.config
-	echo 'CONFIG_EXTRA_FIRMWARE="regulatory.db"' >> $(@D)/.config
-
-	$(SED) '/^\(# \)\?CONFIG_EXTRA_FIRMWARE_DIR\>/d' $(@D)/.config
-	echo 'CONFIG_EXTRA_FIRMWARE_DIR="firmware"' >> $(@D)/.config
-endef
-LINUX_POST_CONFIGURE_HOOKS += LINUX_REGDB_FORCE_BUILTIN_FW_CONFIG
