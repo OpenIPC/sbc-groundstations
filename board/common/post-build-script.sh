3f5e76d3cd10739ddd9cb2240eeb6d5ffb298c67
CC <zhouruixi@gmail.com>
2025-12-25 13:33:22 +0800

    post-build-script: Fix typo (#61)
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- board/common/post-build-script.sh
+++ board/common/post-build-script.sh
@@ -41,7 +41,7 @@ grep -q gadget $TARGET_DIR/etc/inittab || echo '
 
 grep -q "Run customize.sh if it exists" $TARGET_DIR/etc/inittab || echo -e '
 # Run customize.sh if it exists
-::sysinit:/bin/sh -c '\''[ -f /media/dvr/costomize.sh ] && /bin/sh /media/dvr/costomize.sh'\''' >> $TARGET_DIR/etc/inittab
+::sysinit:/bin/sh -c '\''[ -f /media/dvr/customize.sh ] && /bin/sh /media/dvr/customize.sh'\''' >> $TARGET_DIR/etc/inittab
 
 grep -q "framebuffer getty" $TARGET_DIR/etc/inittab || echo '
 # framebuffer getty
