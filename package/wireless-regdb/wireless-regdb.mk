4fe1353cae9535cdbc2930f8734539bd4cb447c8
Ruixi Zhou <zhouruixi@gmail.com>
2025-12-29 16:37:08 +0800

    Add country OO for wifi fpv
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- package/wireless-regdb/wireless-regdb.mk
+++ package/wireless-regdb/wireless-regdb.mk
@@ -1,6 +1,6 @@
 WIRELESS_REGDB_DEPENDENCIES = host-python3 host-openssl
 
 define WIRELESS_REGDB_BUILD_CMDS
-	# patch -p1 -d $(@D) -i ${BR2_EXTERNAL_OPENIPC_SBC_GS_PATH}/package/wireless-regdb/patches/0001-modify-regdb-country-00-for-wifi-fpv.patch
+	patch -p1 -d $(@D) -i ${BR2_EXTERNAL_OPENIPC_SBC_GS_PATH}/package/wireless-regdb/0001-add-country-OO-for-wifi-fpv.patch
 	$(MAKE) -C $(@D) regulatory.db regulatory.db.p7s
 endef
