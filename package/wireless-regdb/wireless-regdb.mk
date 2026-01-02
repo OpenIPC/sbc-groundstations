b19cdc1249f7b6dd2675f92a1678ccd4406e3d44
Henk Wiedig <henk@wiedig.de>
2026-01-02 20:20:23 +0100

    use buildroot's host pyhton

--- package/wireless-regdb/wireless-regdb.mk
+++ package/wireless-regdb/wireless-regdb.mk
@@ -2,5 +2,5 @@ WIRELESS_REGDB_DEPENDENCIES = host-python3 host-openssl
 
 define WIRELESS_REGDB_BUILD_CMDS
 	patch -p1 -d $(@D) -i ${BR2_EXTERNAL_OPENIPC_SBC_GS_PATH}/package/wireless-regdb/0001-add-country-OO-for-wifi-fpv.patch
-	$(MAKE) -C $(@D) regulatory.db regulatory.db.p7s
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) regulatory.db regulatory.db.p7s
 endef
