dcd76e26d798f27f51dab6b98f3efa1ecf1aaa08
Henk Wiedig <henk@wiedig.de>
2026-07-08 21:39:07 +0200

    pp_rk: pass arbirary argument to pixelpilot.sh

--- package/pixelpilot/files/pixelpilot.sh
+++ package/pixelpilot/files/pixelpilot.sh
@@ -3,7 +3,7 @@
 while true
 do
   test -r /etc/default/pixelpilot && . /etc/default/pixelpilot
-  /usr/bin/pixelpilot $PIXELPILOT_ARGS
+  /usr/bin/pixelpilot $PIXELPILOT_ARGS "$@"
   rc=$?
   if [ $rc = 2 -o $rc = 143 -o $rc = 137 ]
   then
