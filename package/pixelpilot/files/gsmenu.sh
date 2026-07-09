2da1f89d5047defed7580602707a3ac64f0dc8b5
Henk Wiedig <henk@wiedig.de>
2026-07-09 16:56:16 +0200

    pp_rk: add auto codec

--- package/pixelpilot/files/gsmenu.sh
+++ package/pixelpilot/files/gsmenu.sh
@@ -661,7 +661,7 @@ case "$@" in
     "get gs system rx_codec")
         . /etc/default/pixelpilot
         echo $PIXELPILOT_CODEC
-        emit_values "h264\nh265"
+        emit_values "h264\nh265\nauto"
         ;;
     "get gs system rx_mode")
         . /etc/default/wifibroadcast
