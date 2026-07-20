004f38de86e1bdab4ab6b303736628641cf7771a
Henk Wiedig <henk@wiedig.de>
2026-07-20 22:38:48 +0200

    pp_rk: bump

--- package/pixelpilot/pixelpilot.mk
+++ package/pixelpilot/pixelpilot.mk
@@ -4,7 +4,7 @@
 #
 ###############################################################################
 
-PIXELPILOT_VERSION=7abb949d764280c8f7ffde2f0e45d9758d46a71d
+PIXELPILOT_VERSION=3833681f18f22bf2a7dd47cba425b3527a44b5e5
 PIXELPILOT_SITE=https://github.com/OpenIPC/PixelPilot_rk.git
 PIXELPILOT_SITE_METHOD = git
 PIXELPILOT_GIT_SUBMODULES = YES
